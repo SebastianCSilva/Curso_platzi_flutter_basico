1544ff879b374dbe37ad2bae3f488edd53b78bca
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-09-24 19:27:56 -0300

    Primer hola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,14 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hola Mundo"),
+        ),
+        body: Center(
+          child: Text("Hola mundo"),
+        ),
+      )//MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
